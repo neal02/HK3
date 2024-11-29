96bef2350c864dcb2138d049a3de2cd97ba293b3
his7067 <95165857+his7067@users.noreply.github.com>
2024-11-29 11:51:55 +0900

    dsda
    sdas

---
+++ Assets/Script/StartGame.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;  // 씬 매니저 관련 기능
+
+public class StartGame : MonoBehaviour
+{
+    void Update()
+    {
+        // 아무 키나 눌렀을 때
+        if (Input.anyKeyDown)
+        {
+            // 다음 씬으로 이동 (씬 이름 사용)
+            SceneManager.LoadScene("jjid");
+        }
+    }
+}
