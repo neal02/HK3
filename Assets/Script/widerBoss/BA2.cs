83efa44e2943d130ae8404ee11e730dc55202a25
jjid <kimeunsr@gmail.com>
2024-12-06 14:48:18 +0900

    S

--- Assets/Script/widerBoss/BA2.cs
+++ Assets/Script/widerBoss/BA2.cs
@@ -33,20 +33,17 @@ public class BA2 : MonoBehaviour
             BoxCollider2D.offset = new Vector2(0.29f, 0);
 
             CapsuleCollider2D.offset = new Vector2((CapsuleCollider2D.offset.x) * -1, CapsuleCollider2D.offset.y);
-            //CapsuleCollider2D.enabled = false
             transform.localPosition = new Vector2(-Mathf.Abs(transform.localPosition.x), transform.localPosition.y);
 
 
         }
-        else if(Bossf ==-1){
+        else if (Bossf == -1) {
             BoxCollider2D.offset = new Vector2(-0.29f, 0);
             CapsuleCollider2D.offset = new Vector2((CapsuleCollider2D.offset.x) * 1, CapsuleCollider2D.offset.y);
             transform.localPosition = new Vector2(Mathf.Abs(transform.localPosition.x), transform.localPosition.y);
-
-            //CapsuleCollider2D.enabled = true;
-
         }
-        bool isAttacking = bossAnimator.GetCurrentAnimatorStateInfo(0).IsName("Attack"); // "BossAttackAnimation"은 애니메이션 이름
+
+        bool isAttacking = bossAnimator.GetCurrentAnimatorStateInfo(0).IsName("Attack"); // "Attack"은 애니메이션 이름
 
         if (isAttacking) {
             if (!isPlayingAttackSound)  // 소리가 재생되지 않았을 경우에만 재생
@@ -56,19 +53,17 @@ public class BA2 : MonoBehaviour
                 AudioManager.instance.PlaySfx(AudioManager.Sfx.Boss_Attack);  // 보스 공격 사운드 바로 재생
                 StartCoroutine(EnableBoxColliderWithDelay(0.5f)); // 0.5초 딜레이 후 BoxCollider 활성화
             }
-            //StartCoroutine(EnableBoxColliderWithDelay(0.5f)); // 1초 딜레이 후 BoxCollider 활성화
         }
         else {
             StopAllCoroutines(); // 공격이 중단되면 코루틴 중지
             BoxCollider2D.enabled = false;
+            isPlayingAttackSound = false;  // 소리 재생 중지
         }
-
     }
 
     private void PlayAttackSound()
     {
-        if (AudioManager.instance != null)
-        {
+        if (AudioManager.instance != null) {
             // 이전 사운드가 끝나지 않아도 새로 시작되게 함
             AudioManager.instance.StopSfx(AudioManager.Sfx.Boss_Attack);  // 이전 사운드 중단
             AudioManager.instance.PlaySfx(AudioManager.Sfx.Boss_Attack);  // 새로 사운드 재생
